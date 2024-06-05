4aafd595532f4a94d65d59621beccdb6dfbb3b46
capitalistspz <keipitalists@proton.me>
2024-06-05 07:37:27 +0100

    Two GamePad support

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 #include <wups/config/WUPSConfigItemBoolean.h>
 #include <wups/config/WUPSConfigItemIntegerRange.h>
 #include <wups/config_api.h>
-#include <vector>
 #include <padscore/wpad.h>
 #include <padscore/kpad.h>
 #include <vpad/input.h>
 #include <coreinit/title.h>
+#include <mutex>
 #include "padscore_extra.h"
 
 WUPS_PLUGIN_NAME("gamepadtopro");
@@ -16,39 +16,67 @@ WUPS_PLUGIN_VERSION("v1.0");
 WUPS_PLUGIN_AUTHOR("capitalistspz");
 WUPS_PLUGIN_LICENSE("MIT");
 
-#define ENABLE_CONFIG_ID "enable"
-#define CHANNEL_CONFIG_ID "channel"
+#define ENABLE_0_CONFIG_ID "enable0"
+#define CHANNEL_0_CONFIG_ID "channel0"
+
+#define ENABLE_1_CONFIG_ID "enable1"
+#define CHANNEL_1_CONFIG_ID "channel1"
 
 WUPS_USE_WUT_DEVOPTAB();
 WUPS_USE_STORAGE("gamepadtopro");
 
-//! Settings
-bool enablePlugin = false;
-int kpadChan = WPAD_CHAN_0;
+struct Setting {
+    bool enable;
+    int channel;
+} settings[2];
 
 //! Info
-VPADChan vpadChan = VPAD_CHAN_0;
 uint64_t applicationTitleId = 0;
 bool configMenuOpen = false;
+
+VPADStatus vpadBuffers[2][32];
+uint32_t vpadBufferCount = 0;
+std::mutex vpadBufferMutex;
 
 bool IsGame(uint64_t id) {
     return (id & 0xFFFFFFFF'00000000ULL) == 0x00050000'00000000ULL;
 }
 
 bool UseReal() {
-    return !enablePlugin || !IsGame(applicationTitleId) || configMenuOpen || !WPADIsEnabledURC();
+    return !IsGame(applicationTitleId) || configMenuOpen || !WPADIsEnabledURC();
+}
+
+bool GetChanIfAllowed(KPADChan chan, VPADChan &vpadChan) {
+    if (UseReal())
+        return false;
+    if (settings[0].enable && settings[0].channel == chan) {
+        vpadChan = VPAD_CHAN_0;
+        return true;
+    } else if (settings[1].enable && settings[1].channel == chan) {
+        vpadChan = VPAD_CHAN_1;
+        return true;
+    }
+    return false;
 }
 
 void channelItemChanged(ConfigItemIntegerRange *item, int newValue) {
-    if (std::string_view(CHANNEL_CONFIG_ID) == item->identifier) {
-        kpadChan = (KPADChan) newValue;
+    if (std::string_view(CHANNEL_0_CONFIG_ID) == item->identifier) {
+        settings[0].channel = newValue;
+        WUPSStorageAPI::Store(item->identifier, newValue);
+    }
+    if (std::string_view(CHANNEL_1_CONFIG_ID) == item->identifier) {
+        settings[1].channel = newValue;
         WUPSStorageAPI::Store(item->identifier, newValue);
     }
 }
 
 void enablePluginItemChanged(ConfigItemBoolean *item, bool newValue) {
-    if (std::string_view(ENABLE_CONFIG_ID) == item->identifier) {
-        enablePlugin = newValue;
+    if (std::string_view(ENABLE_0_CONFIG_ID) == item->identifier) {
+        settings[0].enable = newValue;
+        WUPSStorageAPI::Store(item->identifier, newValue);
+    }
+    if (std::string_view(ENABLE_1_CONFIG_ID) == item->identifier) {
+        settings[1].enable = newValue;
         WUPSStorageAPI::Store(item->identifier, newValue);
     }
 }
@@ -59,14 +87,24 @@ WUPSConfigAPICallbackStatus ConfigMenuOpenedCallback(WUPSConfigCategoryHandle ro
     WUPSConfigCategory root = WUPSConfigCategory(rootHandle);
 
     try {
-        root.add(WUPSConfigItemBoolean::Create(ENABLE_CONFIG_ID, "Enable",
-                                               false, enablePlugin,
+        root.add(WUPSConfigItemBoolean::Create(ENABLE_0_CONFIG_ID, "Gamepad 0 Enable",
+                                               false, settings[0].enable,
                                                &enablePluginItemChanged));
 
-        root.add(WUPSConfigItemIntegerRange::Create(CHANNEL_CONFIG_ID, "Channel",
-                                                    0, kpadChan,
+        root.add(WUPSConfigItemIntegerRange::Create(CHANNEL_0_CONFIG_ID, "Gamepad 0 Channel",
+                                                    0, settings[1].enable,
                                                     0, 6,
                                                     &channelItemChanged));
+
+        root.add(WUPSConfigItemBoolean::Create(ENABLE_1_CONFIG_ID, "Gamepad 1 Enable",
+                                               false, settings[1].enable,
+                                               &enablePluginItemChanged));
+
+        root.add(WUPSConfigItemIntegerRange::Create(CHANNEL_1_CONFIG_ID, "Gamepad 1 Channel",
+                                                    0, settings[1].channel,
+                                                    0, 6,
+                                                    &channelItemChanged));
+
     } catch (std::exception &e) {
         return WUPSCONFIG_API_CALLBACK_RESULT_ERROR;
     }
@@ -83,8 +121,10 @@ INITIALIZE_PLUGIN() {
     WUPSConfigAPIOptionsV1 configOptions = {.name = "gamepadtopro"};
     WUPSConfigAPI_Init(configOptions, ConfigMenuOpenedCallback, ConfigMenuClosedCallback);
 
-    WUPSStorageAPI::GetOrStoreDefault(ENABLE_CONFIG_ID, enablePlugin, false);
-    WUPSStorageAPI::GetOrStoreDefault(CHANNEL_CONFIG_ID, kpadChan, 0);
+    WUPSStorageAPI::GetOrStoreDefault(ENABLE_0_CONFIG_ID, settings[0].enable, false);
+    WUPSStorageAPI::GetOrStoreDefault(CHANNEL_0_CONFIG_ID, settings[0].channel, 0);
+    WUPSStorageAPI::GetOrStoreDefault(ENABLE_1_CONFIG_ID, settings[1].enable, false);
+    WUPSStorageAPI::GetOrStoreDefault(CHANNEL_1_CONFIG_ID, settings[1].channel, 0);
     WUPSStorageAPI::SaveStorage();
 }
 
@@ -97,24 +137,27 @@ ON_APPLICATION_REQUESTS_EXIT() {
 }
 
 DECL_FUNCTION(int32_t, VPADRead, VPADChan chan, VPADStatus *buffers, uint32_t count, VPADReadError *outError) {
-    if (vpadChan != chan || UseReal())
+    if (!settings[chan].enable || UseReal())
         return real_VPADRead(chan, buffers, count, outError);
     bzero(buffers, sizeof(VPADStatus) * count);
     *outError = VPAD_READ_NO_SAMPLES;
+
     return 0;
 }
 
 DECL_FUNCTION(int32_t, KPADReadEx, KPADChan channel, KPADStatus *data, uint32_t size, KPADError *outError) {
-    if (channel != kpadChan || UseReal())
+    VPADChan vpadChan;
+    if (!GetChanIfAllowed(channel, vpadChan))
         return real_KPADReadEx(channel, data, size, outError);
     if (data == nullptr || size == 0) {
         *outError = KPAD_ERROR_NO_SAMPLES;
         return 0;
     }
 
-    std::vector<VPADStatus> buffers(size);
+    auto lock = std::scoped_lock(vpadBufferMutex);
+    vpadBufferCount = std::min(size, 32u);
     VPADReadError error;
-    const auto count = real_VPADRead(vpadChan, buffers.data(), size, &error);
+    const auto count = real_VPADRead(vpadChan, vpadBuffers[vpadChan], vpadBufferCount, &error);
     if (error != VPADReadError::VPAD_READ_SUCCESS || count == 0) {
         switch (error) {
             case VPAD_READ_NO_SAMPLES:
@@ -133,13 +176,13 @@ DECL_FUNCTION(int32_t, KPADReadEx, KPADChan channel, KPADStatus *data, uint32_t 
         return 0;
     }
 
-
-#define COPY_BTNK(dest_btn, src_btn) kpad.pro.hold |= (vpad.hold & src_btn) ? dest_btn : 0; \
-kpad.pro.trigger |= (vpad.trigger & src_btn) ? dest_btn : 0;                                         \
+#define COPY_BTNK(dest_btn, src_btn)                            \
+kpad.pro.hold |= (vpad.hold & src_btn) ? dest_btn : 0;          \
+kpad.pro.trigger |= (vpad.trigger & src_btn) ? dest_btn : 0;    \
 kpad.pro.release |= (vpad.release & src_btn) ? dest_btn : 0
 
     for (auto i = 0; i < count; ++i) {
-        auto const &vpad = buffers[i];
+        auto const &vpad = vpadBuffers[vpadChan][i];
         auto &kpad = data[i];
 
         bzero(&kpad, sizeof(KPADStatus));
@@ -196,31 +239,35 @@ DECL_FUNCTION(int32_t, KPADRead, KPADChan channel, KPADStatus *data, uint32_t si
 }
 
 DECL_FUNCTION(int32_t, WPADProbe, WPADChan chan, WPADExtensionType *outExtensionType) {
-    if (chan != kpadChan || UseReal())
+    VPADChan vpadChan;
+    if (!GetChanIfAllowed(chan, vpadChan))
         return real_WPADProbe(chan, outExtensionType);
     *outExtensionType = WPADExtensionType::WPAD_EXT_PRO_CONTROLLER;
     return 0;
 }
 
 DECL_FUNCTION(void, WPADControlMotor, WPADChan chan, BOOL enabled) {
-    constexpr uint8_t pattern[15] = {0xFF, 0xFF};
-    if (chan != kpadChan || UseReal())
+    constexpr static uint8_t pattern[15] = {0xFF, 0xFF};
+    VPADChan vpadChan;
+    if (!GetChanIfAllowed(chan, vpadChan))
         real_WPADControlMotor(chan, enabled);
     else if (enabled && WPADIsMotorEnabled())
-        VPADControlMotor(vpadChan, (uint8_t *) &pattern, 2);
+        VPADControlMotor((VPADChan) vpadChan, (uint8_t *) &pattern, 2);
     else
-        VPADStopMotor(vpadChan);
+        VPADStopMotor((VPADChan) vpadChan);
 
 }
 
 DECL_FUNCTION(WPADDataFormat, WPADGetDataFormat, WPADChan chan) {
-    if (chan != kpadChan || UseReal())
+    VPADChan vpadChan;
+    if (!GetChanIfAllowed(chan, vpadChan))
         return real_WPADGetDataFormat(chan);
     return WPADDataFormat::WPAD_FMT_PRO_CONTROLLER;
 }
 
 DECL_FUNCTION(void, WPADRead, WPADChan chan, void *buffer) {
-    if (chan != kpadChan || UseReal()) {
+    VPADChan vpadChan;
+    if (!GetChanIfAllowed(chan, vpadChan)) {
         real_WPADRead(chan, buffer);
         return;
     }
@@ -230,16 +277,15 @@ DECL_FUNCTION(void, WPADRead, WPADChan chan, void *buffer) {
     const auto count = real_VPADRead(vpadChan, &vpadStatus, 1, &error);
 
     auto *wpadStatus = (WPADStatusProController *) buffer;
-
     bzero(wpadStatus, sizeof(WPADStatusProController));
-
-    wpadStatus->err = 0;
 
     if (count <= 0 || error != VPAD_READ_SUCCESS) {
         //! no controller
         wpadStatus->err = -1;
+        wpadStatus->extensionType = 0xfd;
         return;
     }
+    wpadStatus->err = 0;
     wpadStatus->extensionType = WPAD_EXT_PRO_CONTROLLER;
     wpadStatus->dataFormat = WPAD_FMT_PRO_CONTROLLER;
 
@@ -285,7 +331,8 @@ DECL_FUNCTION(void, WPADRead, WPADChan chan, void *buffer) {
 }
 
 DECL_FUNCTION(uint8_t, WPADGetBatteryLevel, WPADChan chan) {
-    if (kpadChan != chan || UseReal())
+    VPADChan vpadChan;
+    if (!GetChanIfAllowed(chan, vpadChan))
         return real_WPADGetBatteryLevel(chan);
     // Full battery
     return 4;
